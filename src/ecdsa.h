604189b181520f62adb9ae35c0c290db1e86a0df
dangfan <dangf09@gmail.com>
2019-07-17 20:43:39 +0800

    Reformat code

--- src/ecdsa.h
+++ src/ecdsa.h
@@ -1,20 +1,30 @@
 #ifndef CANOKEY_CRYPTO_SRC_ECDSA_H
 #define CANOKEY_CRYPTO_SRC_ECDSA_H
 
-#include <stdint.h>
 #include <stddef.h>
+#include <stdint.h>
 
 typedef enum {
-  ECDSA_SECP256R1,  // AKA NIST P-256
+  ECDSA_SECP256R1, // AKA NIST P-256
   ECDSA_SECP256K1,
 } ECDSAType;
 
-int ecdsa_sign(ECDSAType ecdsa_type, const uint8_t *priv_key, const uint8_t *digest, uint8_t *sig);
+int ecdsa_sign(ECDSAType ecdsa_type, const uint8_t *priv_key,
+               const uint8_t *digest, uint8_t *sig);
 
-int ecdsa_verify(ECDSAType ecdsa_type, const uint8_t *pub_key, const uint8_t *sig, const uint8_t *digest);
+int ecdsa_verify(ECDSAType ecdsa_type, const uint8_t *pub_key,
+                 const uint8_t *sig, const uint8_t *digest);
 
 int ecdsa_generate(ECDSAType ecdsa_type, uint8_t *priv_key, uint8_t *pub_key);
 
+/**
+ * Convert r,s signature to ANSI X9.62 format
+ *
+ * @param input 64 bytes signature
+ * @param output ANSI X9.62 format. The buffer should be at least 70 bytes. The
+ * buffer can be identical to the input.
+ * @return Length of signature
+ */
 size_t ecdsa_sig2ansi(const uint8_t *input, uint8_t *output);
 
-#endif //CANOKEY_CRYPTO_SRC_ECDSA_H
+#endif // CANOKEY_CRYPTO_SRC_ECDSA_H
--- src/rand.c
+++ src/rand.c
@@ -23,36 +23,12 @@
 
 #include "rand.h"
 
-#ifndef RAND_PLATFORM_INDEPENDENT
-
-#pragma message( \
-    "NOT SUITABLE FOR PRODUCTION USE! Replace random32() function with your own secure code.")
-
-// The following code is not supposed to be used in a production environment.
-// It's included only to make the library testable.
-// The message above tries to prevent any accidental use outside of the test
-// environment.
-//
-// You are supposed to replace the random8() and random32() function with your
-// own secure code. There is also a possibility to replace the random_buffer()
-// function as it is defined as a weak symbol.
-
 static uint32_t seed = 0;
 
-void random_reseed(const uint32_t value) { seed = value; }
-
-uint32_t random32(void) {
-  // Linear congruential generator from Numerical Recipes
-  // https://en.wikipedia.org/wiki/Linear_congruential_generator
+uint32_t __attribute__((weak)) random32(void) {
   seed = 1664525 * seed + 1013904223;
   return seed;
 }
-
-#endif /* RAND_PLATFORM_INDEPENDENT */
-
-//
-// The following code is platform independent
-//
 
 void __attribute__((weak)) random_buffer(uint8_t *buf, size_t len) {
   uint32_t r = 0;
