7e0ccb6756661b7e6f9946c71ae97c899e149f33
dangfan <dangf09@gmail.com>
2019-07-23 15:11:15 +0800

    Add rsa

---
+++ include/rsa.h
@@ -0,0 +1,17 @@
+#ifndef CANOKEY_CORE_CANOKEY_CRYPTO_INCLUDE_RSA_H_
+#define CANOKEY_CORE_CANOKEY_CRYPTO_INCLUDE_RSA_H_
+
+#include <stdint.h>
+
+typedef struct {
+  uint16_t nbits;
+  uint8_t e[4];
+  uint8_t p[128];
+  uint8_t q[128];
+  uint8_t n[256];
+} rsa_key_t;
+
+int rsa_generate_key(rsa_key_t *key, uint16_t nbits);
+int rsa_sign(rsa_key_t *key, void *data, uint16_t len, void *sig);
+
+#endif // CANOKEY_CORE_CANOKEY_CRYPTO_INCLUDE_RSA_H_
---
+++ src/rsa.c
@@ -0,0 +1,38 @@
+#include <mbedtls/rsa.h>
+#include <memory.h>
+#include <rand.h>
+#include <rsa.h>
+
+static int rnd(void *ctx, unsigned char *buf, size_t n) {
+  (void)ctx;
+  random_buffer(buf, n);
+  return 0;
+}
+
+__attribute__((weak)) int rsa_generate_key(rsa_key_t *key, uint16_t nbits) {
+  mbedtls_rsa_context rsa;
+  mbedtls_rsa_init(&rsa, MBEDTLS_RSA_PKCS_V15, 0);
+  if (mbedtls_rsa_gen_key(&rsa, rnd, NULL, nbits, 65537) < 0)
+    return -1;
+  key->nbits = nbits;
+  if (mbedtls_rsa_export_raw(&rsa, key->n, nbits / 8, key->p, nbits / 16,
+                             key->q, nbits / 16, NULL, 0, key->e, 4) < 0)
+    return -1;
+  return 0;
+}
+
+__attribute__((weak)) int rsa_sign(rsa_key_t *key, void *data, uint16_t len,
+                                   void *sig) {
+  mbedtls_rsa_context rsa;
+  mbedtls_rsa_init(&rsa, MBEDTLS_RSA_PKCS_V15, 0);
+  if (mbedtls_rsa_import_raw(&rsa, key->n, key->nbits / 8, key->p,
+                             key->nbits / 16, key->q, key->nbits / 16, NULL, 0,
+                             key->e, 4) < 0)
+    return -1;
+  if (mbedtls_rsa_complete(&rsa) < 0)
+    return -1;
+  if (mbedtls_rsa_rsassa_pkcs1_v15_sign(&rsa, rnd, NULL, MBEDTLS_RSA_PRIVATE,
+                                        MBEDTLS_MD_NONE, len, data, sig) < 0)
+    return -1;
+  return 0;
+}
