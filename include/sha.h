dbd570d4e66d4acbb7c61d9efac5f2d81d5c9f57
dangfan <dangf09@gmail.com>
2019-07-23 20:35:59 +0800

    Remove unused include

--- include/sha.h
+++ include/sha.h
@@ -1,8 +1,7 @@
 #ifndef CANOKEY_CORE_CANOKEY_CRYPTO_SRC_SHA_H_
 #define CANOKEY_CORE_CANOKEY_CRYPTO_SRC_SHA_H_
 
-#include <stdint.h>
 #include "../src/sha2.h"
 #include "../src/sha3.h"
 
-#endif //CANOKEY_CORE_CANOKEY_CRYPTO_SRC_SHA_H_
+#endif // CANOKEY_CORE_CANOKEY_CRYPTO_SRC_SHA_H_
