b74fc204544157184b03096fa22e56c8cf6d1308
Fan DANG <i@dang.fan>
2025-03-03 12:52:59 +0800

    add secp521r1 in algo

--- include/algo.h
+++ include/algo.h
@@ -13,6 +13,7 @@ typedef enum {
   RSA2048,
   RSA3072,
   RSA4096,
+  SECP521R1,
   KEY_TYPE_PKC_END,
   TDEA,
   AES128,
@@ -23,8 +24,8 @@ extern const size_t PRIVATE_KEY_LENGTH[KEY_TYPE_PKC_END];
 extern const size_t PUBLIC_KEY_LENGTH[KEY_TYPE_PKC_END];
 extern const size_t SIGNATURE_LENGTH[KEY_TYPE_PKC_END];
 
-#define IS_ECC(type) ((type) == SECP256R1 || (type) == SECP256K1 || (type) == SECP384R1 || (type) == SM2 || (type) == ED25519 || (type) == X25519)
-#define IS_SHORT_WEIERSTRASS(type) ((type) == SECP256R1 || (type) == SECP256K1 || (type) == SECP384R1 || (type) == SM2)
+#define IS_ECC(type) ((type) == SECP256R1 || (type) == SECP256K1 || (type) == SECP384R1 || (type) == SECP521R1 || (type) == SM2 || (type) == ED25519 || (type) == X25519)
+#define IS_SHORT_WEIERSTRASS(type) ((type) == SECP256R1 || (type) == SECP256K1 || (type) == SECP384R1 || (type) == SECP521R1 || (type) == SM2)
 #define IS_RSA(type) ((type) == RSA2048 || (type) == RSA3072 || (type) == RSA4096)
 
 #endif // CANOKEY_CRYPTO_ALGO_H
