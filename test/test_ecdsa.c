7af8c8050d188805618daed3c2406f33b573add3
Shengqi Chen <harry-chen@outlook.com>
2025-03-09 16:38:02 +0800

    test/ecdsa: refactor tests
    Signed-off-by: Shengqi Chen <harry-chen@outlook.com>

--- test/test_ecdsa.c
+++ test/test_ecdsa.c
@@ -11,34 +11,40 @@
 #include "nist256p1.h"
 #include "crypto-util.h"
 
+#define ECC_TEST_PRELUDE(TYPE) ecc_key_t key; \
+const int KEY_TYPE = TYPE; \
+const size_t KEY_LEN = PUBLIC_KEY_LENGTH[KEY_TYPE]; \
+const size_t SIG_LEN = SIGNATURE_LENGTH[KEY_TYPE]; \
+__attribute__((unused)) uint8_t pub[KEY_LEN]; \
+__attribute__((unused)) uint8_t sig[SIG_LEN]; \
+ecc_generate(KEY_TYPE, &key);
+
 static void test_ecc_keygen(void **state) {
   (void)state;
-  ecc_key_t key;
-  uint8_t expected[64];
-  ecc_generate(SECP256R1, &key);
-  ecdsa_get_public_key(&nist256p1, key.pri, expected);
-  for (int i = 0; i != 64; ++i) {
-    assert_int_equal(key.pub[i], expected[i]);
+  {
+    // Test SECP256R1
+    ECC_TEST_PRELUDE(SECP256R1);
+    uint8_t expected_pubkey[KEY_LEN];
+    ecdsa_get_public_key(&nist256p1, key.pri, expected_pubkey);
+    assert_memory_equal(key.pub, expected_pubkey, KEY_LEN);
   }
 }
 
 static void test_ecdsa_sign(void **state) {
   (void)state;
-  ecc_key_t key;
-  uint8_t sig[64];
-  uint8_t digest[32] = {0x98, 0x34, 0x87, 0x6d, 0xcf, 0xb0, 0x5c, 0xb1, 0x67, 0xa5, 0xc2, 0x49, 0x53, 0xeb, 0xa5, 0x8c,
-                        0x4a, 0xc8, 0x9b, 0x1a, 0xdf, 0x57, 0xf2, 0x8f, 0x2f, 0x9d, 0x09, 0xaf, 0x10, 0x7e, 0xe8, 0xf0};
-  ecc_generate(SECP256R1, &key);
-  ecc_sign(SECP256R1, &key, digest, sizeof(digest), sig);
-  assert_int_equal(ecdsa_verify_digest(&nist256p1, key.pub, sig, digest), 0);
+  {
+    // Test SECP256R1
+    ECC_TEST_PRELUDE(SECP256R1);
+    uint8_t digest[32] = {0x98, 0x34, 0x87, 0x6d, 0xcf, 0xb0, 0x5c, 0xb1, 0x67, 0xa5, 0xc2, 0x49, 0x53, 0xeb, 0xa5, 0x8c,
+                          0x4a, 0xc8, 0x9b, 0x1a, 0xdf, 0x57, 0xf2, 0x8f, 0x2f, 0x9d, 0x09, 0xaf, 0x10, 0x7e, 0xe8, 0xf0};
+    ecc_sign(SECP256R1, &key, digest, sizeof(digest), sig);
+    assert_int_equal(ecdsa_verify_digest(&nist256p1, key.pub, sig, digest), 0);
 
-  memcpy(key.pri, "\x50\x5a\x4f\xcf\xa6\xe2\x20\xba\x55\x09\x58\xab\xc4\xf2\x39\x05\xe9\xdb\x2a\x2b\x5a\xca\x29\xad\x72\x89\x36\x70\x2f\x9a\x69\xea", 32);
-  memcpy(digest, "\x11\x54\xaf\xd4\xf1\x49\x72\x2e\xc5\x90\xdf\x9c\xae\xda\x64\xfe\xef\x82\xcc\x29\xda\x1a\x04\x23\xf1\xf4\xf3\xa4\x8a\x56\x8b\x63", 32);
-  ecc_sign(SM2, &key, digest, sizeof(digest), sig);
-  uint8_t expected[] = {0xf1,0x50,0x7e,0xc1,0x7c,0x63,0x40,0xca,0x2f,0x4c,0x74,0x48,0xa0,0xb2,0x76,0x9f,0xfa,0xe8,0x27,0x01,0x7a,0x2e,0xa9,0xed,0x4e,0x62,0xd7,0x31,0x41,0xd2,0xc9,0x5f,0x80,0xd7,0x92,0x5f,0x9a,0xd9,0xd6,0x11,0x67,0x12,0x9f,0x74,0xca,0x9d,0x4a,0xe5,0xab,0xb1,0x89,0x60,0x3c,0x7b,0xd9,0x6e,0xce,0x77,0xb4,0x45,0xec,0x76,0xf2,0xf7};
-
-  for (int i = 0; i != 64; ++i) {
-    assert_int_equal(sig[i], expected[i]);
+    memcpy(key.pri, "\x50\x5a\x4f\xcf\xa6\xe2\x20\xba\x55\x09\x58\xab\xc4\xf2\x39\x05\xe9\xdb\x2a\x2b\x5a\xca\x29\xad\x72\x89\x36\x70\x2f\x9a\x69\xea", 32);
+    memcpy(digest, "\x11\x54\xaf\xd4\xf1\x49\x72\x2e\xc5\x90\xdf\x9c\xae\xda\x64\xfe\xef\x82\xcc\x29\xda\x1a\x04\x23\xf1\xf4\xf3\xa4\x8a\x56\x8b\x63", 32);
+    ecc_sign(SM2, &key, digest, sizeof(digest), sig);
+    uint8_t expected_sig[] = {0xf1,0x50,0x7e,0xc1,0x7c,0x63,0x40,0xca,0x2f,0x4c,0x74,0x48,0xa0,0xb2,0x76,0x9f,0xfa,0xe8,0x27,0x01,0x7a,0x2e,0xa9,0xed,0x4e,0x62,0xd7,0x31,0x41,0xd2,0xc9,0x5f,0x80,0xd7,0x92,0x5f,0x9a,0xd9,0xd6,0x11,0x67,0x12,0x9f,0x74,0xca,0x9d,0x4a,0xe5,0xab,0xb1,0x89,0x60,0x3c,0x7b,0xd9,0x6e,0xce,0x77,0xb4,0x45,0xec,0x76,0xf2,0xf7};
+    assert_memory_equal(sig, expected_sig, 64);
   }
 }
 
@@ -53,20 +59,13 @@ static void test_ecc_verify_private_key(void **state) {
 
 static void test_ecc_get_public_key(void **state) {
   (void)state;
-  ecc_key_t key;
-  uint8_t expected[64];
-  ecc_generate(SECP256R1, &key);
-  memcpy(expected, key.pub, 64);
-  ecc_complete_key(SECP256R1, &key);
-  for (int i = 0; i != 64; ++i) {
-    assert_int_equal(key.pub[i], expected[i]);
-  }
-
-  ecc_generate(SM2, &key);
-  memcpy(expected, key.pub, 64);
-  ecc_complete_key(SM2, &key);
-  for (int i = 0; i != 64; ++i) {
-    assert_int_equal(key.pub[i], expected[i]);
+  for (int i = 0; i < KEY_TYPE_PKC_END; i++) {
+    if (!IS_ECC(i)) continue;
+    ECC_TEST_PRELUDE(i);
+    uint8_t expected_pubkey[KEY_LEN];
+    memcpy(expected_pubkey, key.pub, KEY_LEN);
+    ecc_complete_key(i, &key);
+    assert_memory_equal(key.pub, expected_pubkey, KEY_LEN);
   }
 }
 
@@ -188,3 +187,5 @@ int main() {
 
   return cmocka_run_group_tests(tests, NULL, NULL);
 }
+
+#undef ECC_TEST_PRELUDE
