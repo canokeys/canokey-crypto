b6c01eae3d73a920ae026b7565f2071308e1c382
Fan DANG <i@dang.fan>
2026-03-24 15:37:26 +0800

    Use explicit hash contexts

--- include/sm3.h
+++ include/sm3.h
@@ -8,9 +8,16 @@
 #define SM3_BLOCK_LENGTH 64
 #define SM3_DIGEST_LENGTH 32
 
-void sm3_init(void);
-void sm3_update(const uint8_t *data, uint16_t len);
-void sm3_final(uint8_t digest[SM3_DIGEST_LENGTH]);
+typedef struct {
+  uint32_t digest[SM3_DIGEST_LENGTH / sizeof(uint32_t)];
+  uint32_t nblocks;
+  uint8_t block[SM3_BLOCK_LENGTH];
+  uint32_t num;
+} sm3_ctx_t;
+
+void sm3_init(sm3_ctx_t *ctx);
+void sm3_update(sm3_ctx_t *ctx, const uint8_t *data, size_t len);
+void sm3_final(sm3_ctx_t *ctx, uint8_t digest[SM3_DIGEST_LENGTH]);
 void sm3_raw(const uint8_t *data, size_t len, uint8_t digest[SM3_DIGEST_LENGTH]);
 
 #endif
--- src/sm3.c
+++ src/sm3.c
@@ -23,92 +23,90 @@
 // SOFTWARE.
 
 #include "sm3.h"
+#include <memzero.h>
 #include <string.h>
 
 #ifdef USE_MBEDCRYPTO
-typedef struct {
-  uint32_t digest[8];
-  int nblocks;
-  unsigned char block[64];
-  int num;
-} sm3_ctx_t;
-
-static sm3_ctx_t ctx;
 static void sm3_compress(uint32_t digest[8], const unsigned char block[64]);
 #endif
 
-__attribute__((weak)) void sm3_init(void) {
+__attribute__((weak)) void sm3_init(sm3_ctx_t *ctx) {
 #ifdef USE_MBEDCRYPTO
-  ctx.digest[0] = 0x7380166F;
-  ctx.digest[1] = 0x4914B2B9;
-  ctx.digest[2] = 0x172442D7;
-  ctx.digest[3] = 0xDA8A0600;
-  ctx.digest[4] = 0xA96F30BC;
-  ctx.digest[5] = 0x163138AA;
-  ctx.digest[6] = 0xE38DEE4D;
-  ctx.digest[7] = 0xB0FB0E4E;
+  ctx->digest[0] = 0x7380166F;
+  ctx->digest[1] = 0x4914B2B9;
+  ctx->digest[2] = 0x172442D7;
+  ctx->digest[3] = 0xDA8A0600;
+  ctx->digest[4] = 0xA96F30BC;
+  ctx->digest[5] = 0x163138AA;
+  ctx->digest[6] = 0xE38DEE4D;
+  ctx->digest[7] = 0xB0FB0E4E;
 
-  ctx.nblocks = 0;
-  ctx.num = 0;
+  ctx->nblocks = 0;
+  ctx->num = 0;
+#else
+  (void)ctx;
 #endif
 }
 
-__attribute__((weak)) void sm3_update(const uint8_t *data, uint16_t len) {
+__attribute__((weak)) void sm3_update(sm3_ctx_t *ctx, const uint8_t *data, size_t len) {
 #ifdef USE_MBEDCRYPTO
-  if (ctx.num) {
-    const unsigned int left = SM3_BLOCK_LENGTH - ctx.num;
+  if (ctx->num) {
+    const unsigned int left = SM3_BLOCK_LENGTH - ctx->num;
     if (len < left) {
-      memcpy(ctx.block + ctx.num, data, len);
-      ctx.num += len;
+      memcpy(ctx->block + ctx->num, data, len);
+      ctx->num += len;
       return;
     } else {
-      memcpy(ctx.block + ctx.num, data, left);
-      sm3_compress(ctx.digest, ctx.block);
-      ctx.nblocks++;
+      memcpy(ctx->block + ctx->num, data, left);
+      sm3_compress(ctx->digest, ctx->block);
+      ctx->nblocks++;
       data += left;
       len -= left;
     }
   }
   while (len >= SM3_BLOCK_LENGTH) {
-    sm3_compress(ctx.digest, data);
-    ctx.nblocks++;
+    sm3_compress(ctx->digest, data);
+    ctx->nblocks++;
     data += SM3_BLOCK_LENGTH;
     len -= SM3_BLOCK_LENGTH;
   }
-  ctx.num = len;
+  ctx->num = len;
   if (len) {
-    memcpy(ctx.block, data, len);
+    memcpy(ctx->block, data, len);
   }
 #else
+  (void)ctx;
   (void)data;
   (void)len;
 #endif
 }
 
-__attribute__((weak)) void sm3_final(uint8_t digest[SM3_DIGEST_LENGTH]) {
+__attribute__((weak)) void sm3_final(sm3_ctx_t *ctx, uint8_t digest[SM3_DIGEST_LENGTH]) {
 #ifdef USE_MBEDCRYPTO
   unsigned int i;
   uint32_t *pdigest = (uint32_t *)digest;
-  uint32_t *count = (uint32_t *)(ctx.block + SM3_BLOCK_LENGTH - 8);
+  uint32_t *count = (uint32_t *)(ctx->block + SM3_BLOCK_LENGTH - 8);
 
-  ctx.block[ctx.num] = 0x80;
+  ctx->block[ctx->num] = 0x80;
 
-  if (ctx.num + 9 <= SM3_BLOCK_LENGTH) {
-    memset(ctx.block + ctx.num + 1, 0, SM3_BLOCK_LENGTH - ctx.num - 9);
+  if (ctx->num + 9 <= SM3_BLOCK_LENGTH) {
+    memset(ctx->block + ctx->num + 1, 0, SM3_BLOCK_LENGTH - ctx->num - 9);
   } else {
-    memset(ctx.block + ctx.num + 1, 0, SM3_BLOCK_LENGTH - ctx.num - 1);
-    sm3_compress(ctx.digest, ctx.block);
-    memset(ctx.block, 0, SM3_BLOCK_LENGTH - 8);
+    memset(ctx->block + ctx->num + 1, 0, SM3_BLOCK_LENGTH - ctx->num - 1);
+    sm3_compress(ctx->digest, ctx->block);
+    memset(ctx->block, 0, SM3_BLOCK_LENGTH - 8);
   }
 
-  count[0] = __builtin_bswap32(ctx.nblocks >> 23);
-  count[1] = __builtin_bswap32((ctx.nblocks << 9) + (ctx.num << 3));
+  count[0] = __builtin_bswap32(ctx->nblocks >> 23);
+  count[1] = __builtin_bswap32((ctx->nblocks << 9) + (ctx->num << 3));
 
-  sm3_compress(ctx.digest, ctx.block);
-  for (i = 0; i < sizeof(ctx.digest) / sizeof(ctx.digest[0]); i++) {
-    pdigest[i] = __builtin_bswap32(ctx.digest[i]);
+  sm3_compress(ctx->digest, ctx->block);
+  for (i = 0; i < sizeof(ctx->digest) / sizeof(ctx->digest[0]); i++) {
+    pdigest[i] = __builtin_bswap32(ctx->digest[i]);
   }
+  memzero(ctx, sizeof(*ctx));
 #else
+  (void)ctx;
   (void)digest;
 #endif
 }
@@ -196,7 +194,8 @@ void sm3_compress(uint32_t digest[8], const unsigned char block[64]) {
 #endif
 
 void sm3_raw(const uint8_t *data, size_t len, uint8_t digest[SM3_DIGEST_LENGTH]) {
-  sm3_init();
-  sm3_update(data, len);
-  sm3_final(digest);
+  sm3_ctx_t ctx;
+  sm3_init(&ctx);
+  sm3_update(&ctx, data, len);
+  sm3_final(&ctx, digest);
 }
